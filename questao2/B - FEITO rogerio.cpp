4fba8afe5d2f730e9e5bb01d0547955784939c76
RogerParrot <rogeriodosanjosbarbosa2004@gmail.com>
2024-09-05 01:05:42 -0300

    subindo questao 2 - rogerio

---
+++ questao2/B - FEITO rogerio.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <regex>
+
+using namespace std;
+
+bool validarFamilia(string familiaStr) {
+    regex regexEmail("(MH|HM)h*mh*(h*mh*mh*)*");
+    return regex_match(familiaStr, regexEmail);
+}
+
+int main() {
+    string familiaStr;
+    char opcao;
+
+    cout << "Deseja prosseguir na operacao? (s/n): ";
+    cin >> opcao;
+
+    while(opcao == 's' || opcao == 'S') {
+        cout << "Insira uma familia: ";
+        cin >> familiaStr;
+
+        if(validarFamilia(familiaStr)) {
+            cout << "A familia foi validada!" << endl;
+        } else {
+            cout << "A familia nao foi validada!" << endl;
+        }
+
+        cout << "Deseja prosseguir na operacao? (s/n): ";
+        cin >> opcao;
+    }
+
+    return 0;
+}
