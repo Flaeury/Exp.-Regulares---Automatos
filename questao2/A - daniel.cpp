e93e7ac5b225776eb43c32c7446c040f1b2ac170
DanielNaiff <danielnaiff344@gmail.com>
2024-09-05 20:46:28 -0300

    questao 2 letra

---
+++ questao2/A - daniel.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <regex>
+#include <string>
+
+int main() {
+    std::regex padrao("(MH|HM)((h|m)*m(h|m)*m(h|m)*|(h|m)*h(h|m)*|(h|m)*h(h|m)*h(h|m)*m(h|m)*)");
+
+    std::string entrada;
+    char continuar;
+
+    do {
+        std::cout << "Insira a cadeia de caracteres que representa a familia: ";
+        std::cin >> entrada;
+
+        if (std::regex_match(entrada, padrao)) {
+            std::cout << "A cadeia corresponde ao padrao de uma familia valida." << std::endl;
+        } else {
+            std::cout << "A cadeia NAO corresponde ao padrao de uma familia valida." << std::endl;
+        }
+
+        std::cout << "Deseja testar outra cadeia? (s/n): ";
+        std::cin >> continuar;
+        
+    } while (continuar == 's' || continuar == 'S');
+
+    return 0;
+}
